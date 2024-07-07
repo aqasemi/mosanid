b1cd2fb323f1d7ae8f0806412bb75bc2c79ed64a
jazzar <jaza@gmail.com>
2024-07-07 14:09:29 +0300

    add db models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Univ(models.Model):
+    uName = models.CharField(max_length=50)
+    adminAcUsername = models.CharField(max_length=200)
+    adminAcApiKey = models.CharField(max_length=200)
+
+class Course(models.Model):
+    userFirstName = models.CharField(max_length=40)
+    username = models.CharField(max_length=40)
+    isStudent = models.BooleanField()
+    univ = models.ForeignKey(Univ, on_delete=models.CASCADE)
+    learningMat = models.FileField(upload_to=None, max_length=254)
+
+class user(models.Model):
+    userFirstName = models.CharField(max_length=40)
+    username = models.CharField(max_length=40)
+    isStudent = models.BooleanField()
+    univ = models.ForeignKey(Univ, on_delete=models.CASCADE)
+    Courses = models.ManyToManyField(Course)
+
